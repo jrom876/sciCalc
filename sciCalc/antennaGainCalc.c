2940c1e4879ddbe76bd6ff6e2fd905574d9cc761
jrom876 <jrom876@gmail.com>
2021-05-09 23:23:38 -0600

    Update antennaGainCalc.c

--- sciCalc/antennaGainCalc.c
+++ sciCalc/antennaGainCalc.c
@@ -1,4 +1,7 @@
-// antennaGainCalc.c //
+
+// File:		antennaGainCalc.c 
+// Purpose:		Calculate Antenna Gain and convert freq <==> wavelength
+// Author:		jrom876
 
 #include <stdint.h>
 #include <string.h>
@@ -17,26 +20,42 @@
 float eta       = 0; // antenna efficiency - %
 float lambda    = 0; // wavelength - meters
 float aperature = 0; // antenna aperature - meters^2
-float frequency = 0;
+float frequency = 0; // frequency - hertz
 float gain      = 0;
 float tempf;
 //int exitFlag = 0;
 
 
-float calcAntGain(float et, float ap, float fr){
+float calcAntGainFromFreq(float et, float ap, float fr){
   float lam = calcLambda(fr);
   float result = ((10*log10((et*4*PI*ap)/(lam*lam))));
-  printf("Parabolc Antenna gain = %f\n",result);
+  printf("Parabolc Antenna gain = %f\n\n",result);
   eta = et; aperature = ap; frequency = fr;
   gain = result; tempf = result;
   return result;
 }
 
+float calcAntGainFromLambda(float et, float ap, float lam){
+  float frq = calcFreq(lam);
+  float result = (10*log10((et*4*PI*ap)/(lam*lam)));
+  printf("Parabolc Antenna gain = %f\n\n",result);
+  eta = et; aperature = ap; frequency = frq;
+  gain = result; tempf = result;
+  return result;
+}
+
 float calcLambda(float freq){
-  float clambda = LIGHT_SPEED/freq;
-  printf("wavelength = %f \n",clambda);
+  float clambda = LIGHT_SPEED/(freq*1000000);
+  printf("\nwavelength = %f \n",clambda);
   tempf = clambda;
   return clambda;
+}
+
+float calcFreq(float lam){
+  float freq = LIGHT_SPEED/lam;
+  printf("\nfrequency = %f \n",freq);
+  tempf = freq;
+  return freq;
 }
 
 void printLambda(){
@@ -44,11 +63,16 @@ void printLambda(){
   printf("wavelength = %f meters\n",lambda);
 }
 
+void printFreq(){
+  //frequency = LIGHT_SPEED/lambda;
+  printf("wavelength = %f meters\n",frequency);
+}
+
 ////////////////////////////////////////////
-/// The following 3 functions create a .txt
-/// file if it does not already exist; convert an
-/// input float to a string and append the .txt
-/// file with that string on a new line; and
+// The following 3 functions create a .txt
+// file if it does not already exist; convert an
+// input float to a string and append the .txt
+// file with that string on a new line; and
 // clear all data from the .txt file if requested.
 ////////////////////////////////////////////
 char * floatToStringAG(float f){
@@ -76,8 +100,10 @@ int getUserInputAG(){
 	float e, a, f, l;
 	int var;
 	printf("Please choose from the following: \n");
-	printf("1 for calcAntGain()\n");
-	printf("2 for calcLambda(freq)\n");
+	printf("1 for calcAntGainFromFreq(freq)\n");
+	printf("2 for calcAntGainFromLambda(lam)\n");
+	printf("3 for calcLambda(freq)\n");
+	printf("4 for calcFreq(lam)\n");
 	// printf("3 for ftoc\n");
 	// printf("4 for ftok\n");
 	// printf("5 for ktoc\n");
@@ -87,29 +113,42 @@ int getUserInputAG(){
 	scanf(" %i", &var);
 	switch (var) {
 		case 1:
-		        printf("Please enter efficiency: \n");
+			printf("Calculating Antenna Gain using frequency\n");
+		    printf("Please enter efficiency: \n");
 			scanf(" %f", &e);
-		        printf("Please enter aperature area in m^2: \n");
-		        scanf(" %f", &a);
-		        printf("Please enter frequency in Hz: \n");
-		        scanf(" %f", &f);
-			calcAntGain(e, a, f);
+		    printf("Please enter aperature area in m^2: \n");
+		    scanf(" %f", &a);
+		    printf("Please enter frequency in MHz: \n");
+		    scanf(" %f", &f);
+			calcAntGainFromFreq(e, a, f);
 			break;
 		case 2:
+			printf("Calculating Antenna Gain using wavelength\n");
+		    printf("Please enter efficiency: \n");
+			scanf(" %f", &e);
+		    printf("Please enter aperature area in m^2: \n");
+		    scanf(" %f", &a);
+		    printf("Please enter wavelength in meters: \n");
+		    scanf(" %f", &l);
+			calcAntGainFromLambda(e, a, l);
+			break;
+		case 3:
 			printf("Calculating Lambda\n");
 			printf("Please enter frequency: \n");
 			scanf(" %f", &f);
 			calcLambda(f);
 			break;
-		case 3:
-			break;
 		case 4:
+			printf("Calculating Frequency\n");
+			printf("Please enter wavelength: \n");
+			scanf(" %f", &l);
+			calcFreq(l);
 			break;
 		case 5:
 			break;
 		case 6:
 			clearDataFromFileAG();
-      			printf("antennaGainData.txt is now cleared of all data");
+      		printf("antennaGainData.txt is now cleared of all data");
 			break;
 		case 7:
 			printf("Writing data to file\n");
