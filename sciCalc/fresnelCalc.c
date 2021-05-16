41a6cb52391bb93cb8da206f427d77cf163f8ed3
jrom876 <jrom876@gmail.com>
2021-05-15 22:03:39 -0600

    Update fresnelCalc.c

--- sciCalc/fresnelCalc.c
+++ sciCalc/fresnelCalc.c
@@ -1,3 +1,32 @@
+//	Package:	sciCalc
+//	File:		fresnelCalc.c 
+//	Purpose:	Calculate Fresnel Zone diameter and radius
+//	Author:		jrom876
+
+/**
+	Copyright (C) 2019, 2021 
+	Jacob Romero, Creative Engineering Solutions, LLC
+	cesllc876@gmail.com
+	admin@jrom.io 
+
+	This program is free software; you can redistribute it
+	and/or modify it under the terms of the GNU General Public  
+	License as published by the Free Software Foundation, version 2.
+
+	This program is distributed in the hope that it will be
+	useful, but WITHOUT ANY WARRANTY; without even the implied 
+	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	
+	See the GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public
+	License along with this program; if not, write to:
+	The Free Software Foundation, Inc.
+	59 Temple Place, Suite 330
+	Boston, MA 02111-1307 USA
+
+**/
+
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
@@ -13,12 +42,26 @@ float freqGHz;
 float tempf;
 
 // Functions //
-float calcFresnelRadius( float freqG, float distKM ) {
-	float result = (17.31*sqrt(distKM/(4*freqG)))/2;
-	printf("%.4f\n", result);
+float calcFresnelDiameter( float freqG, float distKM ) {
+	float result = (17.31*sqrt(distKM/(4*freqG)));
+	printf("\nFresnel Diameter = %.4f meters\n\n", result);
 	tempf = result;
 	return result;
 }
+
+float calcFresnelRadius( float freqG, float distKM ) {
+	float result = (8.656*sqrt(distKM/(4*freqG)));
+	printf("\nFresnel Radius = %.4f meters\n\n", result);
+	tempf = result;
+	return result;
+}
+
+//~ float calcFresnelInterference( float freqG, float distRKM, float distDKM ) {
+	//~ float result = (8.656*sqrt(distKM/(4*freqG)));
+	//~ printf("\nFresnel Radius = %.4f meters\n\n", result);
+	//~ tempf = result;
+	//~ return result;
+//~ }
 
 char * floatToStringFC(float f){
 	char buffer[DATA_SIZE];
@@ -44,7 +87,8 @@ int getUserInputFC(){
 	float dkm, fg;
 	int var;
 	printf("Please choose from the following: \n");
-	printf("1 for fresnel zone radius (m)\n");
+	printf("1 for fresnel zone diameter (m)\n");
+	printf("2 for fresnel zone radius (m)\n");
 	printf("6 to clear all data from fresnelData.txt\n");
 	printf("7 for store to file\n");
 	printf("8 for quit\n");
@@ -55,10 +99,14 @@ int getUserInputFC(){
 			scanf(" %f", &dkm);
 			printf("Please enter frequency in GHz: \n");
 			scanf(" %f", &fg);
-			calcFresnelRadius(fg, dkm);
+			calcFresnelDiameter(fg, dkm);
 			break;
 		case 2:
-
+			printf("Please enter distance in km: \n");
+			scanf(" %f", &dkm);
+			printf("Please enter frequency in GHz: \n");
+			scanf(" %f", &fg);
+			calcFresnelRadius(fg, dkm);
 			break;
 		case 3:
 
@@ -83,6 +131,7 @@ int getUserInputFC(){
 			break;
 		default:
 			printf("Wrong choice. Now exiting.\n");
+			exit(0);
 			exitFlag = 1;
 			break;
 	}
@@ -90,8 +139,8 @@ int getUserInputFC(){
 	getUserInputFC();
 }
 
-// int main(int argc, char const *argv[]) {
-//   calcFresnelRadius( 24, 50 ) ;
-//   getUserInputFC();
-//   return 0;
-// }
+//~ int main(int argc, char const *argv[]) {
+  //~ calcFresnelRadius( 24, 50 ) ;
+  //~ getUserInputFC();
+   //~ return 0;
+//~ }
