bb49197db0a5a316cf85ba676c8ea831c9a65cc0
jrom876 <jrom876@gmail.com>
2021-05-15 21:59:05 -0600

    Update antennaGainCalc.h

--- sciCalc/antennaGainCalc.h
+++ sciCalc/antennaGainCalc.h
@@ -1,8 +1,38 @@
-// antennaGainCalc.h //
+// antennaGainCalc.h 
+#ifndef ANTENNAGAINCALC_H
+#define ANTENNAGAINCALC_H
+
+/**
+	Copyright (C) 2019, 2021 
+	Jacob Romero, Creative Engineering Solutions, LLC
+	cesllc876@gmail.com
+	admin@jrom.io 
+
+	This program is free software; you can redistribute it
+	and/or modify it under the terms of the GNU General Public  
+	License as published by the Free Software Foundation, version 2.
+
+	This program is distributed in the hope that it will be
+	useful, but WITHOUT ANY WARRANTY; without even the implied 
+	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	
+	See the GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public
+	License along with this program; if not, write to:
+	The Free Software Foundation, Inc.
+	59 Temple Place, Suite 330
+	Boston, MA 02111-1307 USA
+
+**/
 
 char * floatToStringAG(float f);
 void storeDataInFileAG(float input);
 int getUserInputAG(void);
 void clearDataFromFileAG(void);
-float calcAntGain(float et, float ap, float fr);
+float calcAntGainFromFreq(float et, float ap, float fr);
 float calcLambda(float freq);
+float calcAntGainFromLambda(float et, float ap, float lam);
+float calcFreq(float lam);
+
+#endif
