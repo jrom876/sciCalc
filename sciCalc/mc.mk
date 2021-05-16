f3c6e0cf2f9a1dea7e151ec39b0cbd7405fcc90c
jrom876 <jrom876@gmail.com>
2021-05-15 22:08:41 -0600

    Update mc.mk

--- sciCalc/mc.mk
+++ sciCalc/mc.mk
@@ -6,20 +6,26 @@ CC=gcc
 DEPS = tempCalc.h \
 	vswrCalc.h \
 	ustripZCalc.h \
+	bpf2Calc.h \
 	fresnelCalc.h \
 	antennaGainCalc.h \
 	linkBudgetCalc.h \
 	inrushICalc.h \
-	decToBinCalc.h
+	decToBinCalc.h \
+	primeNumsCalc.h \
+	complexCalc.h
 OBJ = main.o \
 	tempCalc.o \
 	vswrCalc.o \
 	ustripZCalc.o \
+	bpf2Calc.o \
 	fresnelCalc.o \
 	antennaGainCalc.o \
 	linkBudgetCalc.o \
 	inrushICalc.o \
-	decToBinCalc.o
+	decToBinCalc.o \
+	primeNumsCalc.o \
+	complexCalc.o
 DEBUG=-g
 #LIBS=-lm
 LIBS=-lpthread -lm -lrt
@@ -27,7 +33,7 @@ LIBS=-lpthread -lm -lrt
 #*** Targets ***
 
 %.o: %.c $(DEPS)
-	$(CC) -c -o $@ $< $(LIBS)
+	$(CC) -c -o $@ $< $(LIBS) 
 
 main: $(OBJ)
 	$(CC) -o $@ $^ $(LIBS)
