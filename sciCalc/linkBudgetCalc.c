cc39cdd0045c4f3653ab5b5bfbb4fccf36c51dfa
jrom876 <jrom876@gmail.com>
2021-05-15 22:06:31 -0600

    Update linkBudgetCalc.c

--- sciCalc/linkBudgetCalc.c
+++ sciCalc/linkBudgetCalc.c
@@ -1,24 +1,110 @@
+//	Package:	sciCalc
+//	File:		linkBudgetCalc.c
+// 	Purpose:	Calculate Link Budget (One-way and Two-way path losses)
+// 	Author:		jrom876
+
+/**
+	Copyright (C) 2019, 2021 
+	Jacob Romero, Creative Engineering Solutions, LLC
+	cesllc876@gmail.com
+	admin@jrom.io 
+
+	This program is free software; you can redistribute it
+	and/or modify it under the terms of the GNU General Public  
+	License as published by the Free Software Foundation, version 2.
+
+	This program is distributed in the hope that it will be
+	useful, but WITHOUT ANY WARRANTY; without even the implied 
+	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	
+	See the GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public
+	License along with this program; if not, write to:
+	The Free Software Foundation, Inc.
+	59 Temple Place, Suite 330
+	Boston, MA 02111-1307 USA
+
+**/
+
+// https://www.random-science-tools.com/electronics/friis.htm
+// https://www.antenna-theory.com/basics/friis.php
+// https://rfcafe.com/references/electrical/ew-radar-handbook/two-way-radar-equation.htm
+
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include "linkBudgetCalc.h"
+#define PI            3.14159265358979323846 // ad infinitum
+#define LIGHT_SPEED   299792458 // meters per second
 #define DATA_SIZE 1000
+#define KILO 1000
+#define MEGA 1000000
+#define GIGA 1000000000
+#define TERA 1000000000000 
 #define TXT_FILE "linkBudgetData.txt"
 //
-/// Globals ///
+// Globals //
 float freqGHz, txpwrW, distTargetKM;
 float radarXSect, antGain;
-float hmm, wmm, tmm;
+float plf = 0; // Polarization Loss Factor
 float tempf;
 //int exitFlag = 0;
 
-/// Functions ///
+//////////////////////////
+/////// TX ANTENNA ///////
+//////////////////////////
+//// TX Power Density ////
+//	p = Pt/(4Pi*R^2) * Gt 
+//// power density = TX power * Gain
+
+//////////////////////////
+/////// RX ANTENNA ///////
+//////////////////////////
+
+//// RX Power Density ////
+//	Pr = Pt/(4Pi*R^2) * Gt * Aer 
+//// Pr = TX power * Gain * Effective Aperature Area of RX antenna (m^2)
+
+//// Effective Aperatue Area ////
+// Aer = (lambda^2/4Pi) * Gr 
+//// Aer = (c^2/(4Pi*freq^2)) * Gr
+
+//// Friis Transmission Formulas ////
+
+// Pr = Pt * Gt * Gr * lambda^2 / (4Pi*R)^2
+// Pr = Pt * Gt * Gr * c^2 / (4Pi*R*freq)^2
+
+// Functions //
+/**
+ * Two-way Path Loss, Monostatic
+ * **/
 
 float twoWayPathLossDB(float freq, float dist, float rxs){
   float result = ((20*log(freq))+(40*log(dist))+163.43-(10*log(rxs)))*(-1);
   printf("Path Loss (dB): \t%.5f\n",result);
+  tempf = result;
+  return result;
+}
+
+/** 
+ * Received Signal at Target Equations
+**/
+
+float rxPwrWatts(float Pt, float GtdB, float GrdB, float R, float freq){	
+  float result = Pt*pow(10,log10(GtdB))*pow(10,log10(GrdB))
+		*(pow(LIGHT_SPEED,2))/(pow((4*PI*(R*KILO)*(freq*GIGA)),2));
+  printf("RX Power at antenna: \t%.8f pico-watts\n",result*TERA);
+  tempf = result;
+  return result;
+}
+
+float rxPwrDBM(float PtdBm, float GtdB, float GrdB, float R, float freq){	
+  float result = pow(10,((PtdBm-30)/10))*pow(10,log10(GtdB))
+		*pow(10,log10(GrdB))*(pow(LIGHT_SPEED,2))/(pow((4*PI*(R*KILO)*(freq*GIGA)),2));
+  printf("RX Power at antenna: \t%.8f pico-watts\n",result*TERA);
   tempf = result;
   return result;
 }
@@ -45,11 +131,13 @@ void clearDataFromFileLB(){
 }
 
 int getUserInputLB(){
-	float f, d, xs;
+	float f, d, xs, txp, gtdb, grdb;
   	int exitFlag = 0;
 	int var;
 	printf("Please choose from the following: \n");
 	printf("1 to calculate twoWayPathLossDB \n");
+	printf("2 to calculate rxPwrWatts \n");
+	printf("3 to calculate rxPwrDBM \n");
   	printf("6 to clear all data from linkBudgetData.txt\n");
 	printf("7 for store to file\n");
 	printf("8 for quit\n");
@@ -65,10 +153,31 @@ int getUserInputLB(){
 			twoWayPathLossDB(f,d,xs);
       			break;
 		case 2:
+			printf("Please enter freq in Ghz: \n");
+			scanf(" %f", &f);
+			printf("Please enter distance in km: \n");
+			scanf(" %f", &d);
+			printf("Please enter TX power in Watts: \n");
+			scanf(" %f", &txp);
+			printf("Please enter TX Antenna Gain in dB: \n");
+			scanf(" %f", &gtdb);
+			printf("Please enter RX Antenna Gain in dB: \n");
+			scanf(" %f", &grdb);
+			rxPwrWatts(txp,gtdb,grdb,d,f);
 
 			break;
 		case 3:
-
+			printf("Please enter freq in Ghz: \n");
+			scanf(" %f", &f);
+			printf("Please enter distance in km: \n");
+			scanf(" %f", &d);
+			printf("Please enter TX power in dBm: \n");
+			scanf(" %f", &txp);
+			printf("Please enter TX Antenna Gain in dB: \n");
+			scanf(" %f", &gtdb);
+			printf("Please enter RX Antenna Gain in dB: \n");
+			scanf(" %f", &grdb);
+			rxPwrDBM(txp,gtdb,grdb,d,f);
 			break;
 		case 6:
 			clearDataFromFileLB();
@@ -85,6 +194,7 @@ int getUserInputLB(){
 		default:
 			printf("Wrong choice. Now exiting.\n");
 			exitFlag = 1;
+			exit(0);
 			break;
 	}
 	if(0 != exitFlag) return 0;
