9cc86c158c97ff6483d72ca4e1f4b8573e1a4e78
jrom876 <jrom876@gmail.com>
2021-05-15 22:09:56 -0600

    Update tempCalc.h

--- sciCalc/tempCalc.h
+++ sciCalc/tempCalc.h
@@ -1,4 +1,31 @@
-// tempCalc.h //
+// tempCalc.h 
+#ifndef TEMPCALC_H
+#define TEMPCALC_H
+
+/**
+	Copyright (C) 2019, 2021 
+	Jacob Romero, Creative Engineering Solutions, LLC
+	cesllc876@gmail.com
+	admin@jrom.io 
+
+	This program is free software; you can redistribute it
+	and/or modify it under the terms of the GNU General Public  
+	License as published by the Free Software Foundation, version 2.
+
+	This program is distributed in the hope that it will be
+	useful, but WITHOUT ANY WARRANTY; without even the implied 
+	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	
+	See the GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public
+	License along with this program; if not, write to:
+	The Free Software Foundation, Inc.
+	59 Temple Place, Suite 330
+	Boston, MA 02111-1307 USA
+
+**/
+
 char * floatToStringTC(float f);
 void storeDataInFileTC(float input);
 int getUserInputTC(void);
@@ -9,3 +36,5 @@ float ftoc(float);
 float ftok(float);
 float ktoc(float);
 float ktof(float);
+
+#endif
