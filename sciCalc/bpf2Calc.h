ffb9651e2c2856fde7d5a8baa6123c9678cb7fa8
jrom876 <jrom876@gmail.com>
2021-05-15 22:00:47 -0600

    Update bpf2Calc.h

--- sciCalc/bpf2Calc.h
+++ sciCalc/bpf2Calc.h
@@ -1,33 +1,129 @@
-// bpf2Calc.h //
-// #ifndef BPF2CALC_H
-// #define BPF2CALC_H
+// bpf2Calc.h 
+#ifndef BPF2CALC_H
+#define BPF2CALC_H
 
-char * floatToStringBPF(float f);
-void storeDataInFileBPF(float input);
-int getUserInputBPF(void);
-void clearDataFromFileBPF(void);
+/**
+	Copyright (C) 2019, 2021 
+	Jacob Romero, Creative Engineering Solutions, LLC
+	cesllc876@gmail.com
+	admin@jrom.io 
 
-#define PI        3.14159265358979323846
-#define DATA_SIZE 1000
+	This program is free software; you can redistribute it
+	and/or modify it under the terms of the GNU General Public  
+	License as published by the Free Software Foundation, version 2.
+
+	This program is distributed in the hope that it will be
+	useful, but WITHOUT ANY WARRANTY; without even the implied 
+	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	
+	See the GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public
+	License along with this program; if not, write to:
+	The Free Software Foundation, Inc.
+	59 Temple Place, Suite 330
+	Boston, MA 02111-1307 USA
+
+**/
+
+float calcQbp(float cf, float bw);
+void setKQValues(int choice);
+
+float calcQ1(void);
+float calcQ2(void);
+float calcK12(void);
+float calcCNode2(float cf, float ind);
+float calcC12(void);
+float calcC1(void);
+float calcC2(void);
+float calcR1(void);
+float calcR2(void);
+void computeValues_2(int ch);
+
+float calcQ1_3(void);
+float calcQ3_3(void);
+float calcK12_3(void);
+float calcK23_3(void);
+float calcCNode3(float cf, float ind);
+float calcC12_3(void);
+float calcC23_3(void);
+float calcC1_3(void);
+float calcC2_3(void);
+float calcC3_3(void);
+float calcR1_3(void);
+float calcR2_3(void);
+void computeValues_3(int ch);
+
+float printInductorValue(void);
+char * floatToStringBPF2(float f);
+void storeDataInFileBPF2(float input);
+int getUserInputBPF2(void);
+void clearDataFromFileBPF2(void);
+
+#define PI        	3.14159265358979323846
+#define DATA_SIZE 	1000
 //#define OBP       2
 
-#define Q1BW      1.414 // Butterworth k-q values
-#define K12BW     0.707
-#define Q2BW      1.414
+// 2 element filters
+//~ Filter Type 			q1 		k12 	q2
+//~ Butterworth 			1.414 	0.707 	1.414
+//~ Chebychev 0.1dB ripple 	1.638 	0.711 	1.638
+//~ Chebychev 0.5dB ripple 	1.950 	0.723 	1.950
+//~ Chebychev 1dB ripple 	2.210 	0.739 	2.210
+//~ Bessel 					0.5755 	0.900 	2.148
 
-#define Q1CH_01DB 1.638 // Chebychev 0.1dB k-q values
-#define K12CH_01DB 0.711
-#define Q2CH_01DB 1.638
+#define Q1BW      	1.414 // Butterworth k-q values
+#define K12BW     	0.707
+#define Q2BW      	1.414
 
-#define Q1CH_05DB 1.95  // Chebychev 0.5dB k-q values
-#define K12CH_05DB 0.723
-#define Q2CH_05DB 1.95
+#define Q1CH_01DB 	1.638 // Chebychev 0.1dB k-q values
+#define K12CH_01DB 	0.711
+#define Q2CH_01DB 	1.638
 
-#define Q1CH_1DB  2.21  // Chebychev 1.0dB k-q values
-#define K12CH_1DB 0.729
-#define Q2CH_1DB  2.21
+#define Q1CH_05DB 	1.95  // Chebychev 0.5dB k-q values
+#define K12CH_05DB 	0.723
+#define Q2CH_05DB 	1.95
 
-#define Q1BES     0.5755 // Bessel k-q values
-#define K12BES    0.9
-#define Q2BES     2.148
-// #endif
+#define Q1CH_1DB  	2.21  // Chebychev 1.0dB k-q values
+#define K12CH_1DB 	0.729
+#define Q2CH_1DB  	2.21
+
+#define Q1BES     	0.5755 // Bessel k-q values
+#define K12BES    	0.9
+#define Q2BES     	2.148
+
+// 3 element filters
+//~ Filter Type 			q1 		k12 	k23 	q3
+//~ Butterworth 			1 		0.707 	0.707 	1
+//~ Chebychev 0.1dB ripple 	1.433 	0.662 	0.662 	1.433
+//~ Chebychev 0.5dB ripple 	1.864 	0.647 	0.647 	1.864
+//~ Chebychev 1dB ripple 	2.210 	0.638 	0.638 	2.210
+//~ Bessel 					0.337 	1.748 	0.684 	2.203
+//~ 
+
+#define Q1BW_3    		1.0 // Butterworth k-q values
+#define K12BW_3     	0.707
+#define K23BW_3     	0.707
+#define Q3BW_3      	1.0
+
+#define Q1CH_01DB_3 	1.433 // Chebychev 0.1dB k-q values
+#define K12CH_01DB_3 	0.662
+#define K23CH_01DB_3 	0.662
+#define Q3CH_01DB_3		1.433
+
+#define Q1CH_05DB_3 	1.864  // Chebychev 0.5dB k-q values
+#define K12CH_05DB_3 	0.647
+#define K23CH_05DB_3 	0.647
+#define Q3CH_05DB_3 	1.864
+
+#define Q1CH_1DB_3  	2.21  // Chebychev 1.0dB k-q values
+#define K12CH_1DB_3 	0.638
+#define K23CH_1DB_3 	0.638
+#define Q3CH_1DB_3  	2.21
+
+#define Q1BES_3     	0.337 // Bessel k-q values
+#define K12BES_3    	1.748
+#define K23BES_3    	0.684
+#define Q3BES_3     	2.203
+
+#endif
