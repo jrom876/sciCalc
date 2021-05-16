ba9c22508e0021c3ca089b30dcac4dd370fb3d60
jrom876 <jrom876@gmail.com>
2021-05-15 22:04:48 -0600

    Update inrushICalc.c

--- sciCalc/inrushICalc.c
+++ sciCalc/inrushICalc.c
@@ -1,4 +1,32 @@
-// inrushCalc.c //
+//	Package:	sciCalc
+//	File:		inrushCalc.c 
+//	Purpose:	Calculate Power Supply Inrush Current
+//	Author:		jrom876
+
+/**
+	Copyright (C) 2019, 2021 
+	Jacob Romero, Creative Engineering Solutions, LLC
+	cesllc876@gmail.com
+	admin@jrom.io 
+
+	This program is free software; you can redistribute it
+	and/or modify it under the terms of the GNU General Public  
+	License as published by the Free Software Foundation, version 2.
+
+	This program is distributed in the hope that it will be
+	useful, but WITHOUT ANY WARRANTY; without even the implied 
+	warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	
+	See the GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public
+	License along with this program; if not, write to:
+	The Free Software Foundation, Inc.
+	59 Temple Place, Suite 330
+	Boston, MA 02111-1307 USA
+
+**/
+
 // https://e2e.ti.com/blogs_/b/powerhouse/archive/2015/03/31/
 // powertips-how-to-limit-inrush-current-in-an-ac-dc-power-supply
 #include <stdint.h>
