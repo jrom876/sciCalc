0b386a381f2943508591e09167ea3f5fadf6f4b0
jrom876 <jrom876@gmail.com>
2024-12-22 11:20:24 -0700

    Update pssr.h

--- sciCalc-master/sciCalc/pssr.h
+++ sciCalc-master/sciCalc/pssr.h
@@ -3,10 +3,9 @@
 #define PSSR_H
 
 /**
-	Copyright (C) 2019, 2021 
+	Copyright (C) 2024 
 	Jacob Romero, Creative Engineering Solutions, LLC
 	cesllc876@gmail.com
-	admin@jrom.io 
 
 	This program is free software; you can redistribute it
 	and/or modify it under the terms of the GNU General Public  
