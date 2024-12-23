60fdb2d1122cfcdadd911ef1a61ea8032fb14520
jrom876 <jrom876@gmail.com>
2024-12-23 07:10:56 -0700

    Update inrushICalc.c

--- sciCalc-master/sciCalc/inrushICalc.c
+++ sciCalc-master/sciCalc/inrushICalc.c
@@ -8,7 +8,6 @@
 	Copyright (C) 2019, 2021 
 	Jacob Romero, Creative Engineering Solutions, LLC
 	cesllc876@gmail.com
-	admin@jrom.io 
 
 	This program is free software; you can redistribute it
 	and/or modify it under the terms of the GNU General Public  
@@ -41,9 +40,40 @@
 #include <stdlib.h>
 #include <math.h>
 #include "inrushICalc.h"
+
+/// STANDARD DEFINITIONS FOR PROJECT SCICALC 
+#define PI	3.14159265358979323846 	// ad infinitum sine repeto
+#define LIGHT_SPEED		299792458.0 	// meters per second
+#define STACK_OVERFLOW	2147483648		// Hex 0x80000000
 #define DATA_SIZE 1000
+#define DELTA 1.0e-6
+#define MILLI 1.0e-3
+#define MICRO 1.0e-6
+#define NANO 1.0e-9
+#define PICO 1.0e-12
+#define FEMTO 1.0e-15
+#define ATTO 1.0e-18
+#define KILO 1.0e3
+#define MEGA 1.0e6
+#define GIGA 1.0e9
+#define TERA 1.0e12 
+#define true 1
+#define false 0
+
+/// STANDARD DEFINITIONS FOR LIGHT INTENSITY AND ELECTRIC FIELD CALCULATIONS
+#define AIR_REFRACTIVE_INDEX 1.00027717
+#define E0 8.8541878128*PICO				// Permittivity of Free Space in Farads per meter
+#define MU0 1.25663706212*MICRO				// Permeability of Free Space in Newtons per square meter
+#define EPSILON_0 1/(MU0*(LIGHT_SPEED**2))	// Permittivity of Free Space Equation
+#define E_CONSTANT 1/(4*PI*EPSILON_0)
+#define ELECTRON_CHARGE 1.6e-19 			// Charge of an electron in Coulombs
+#define RADIUS_HELIUM_ATOM 26.5e-12			// Radius of a Helium atom in meters
+#define LED_ARRAY_RADIUS 0.35 				// meters from LED array to sample plate
+#define ETA_T (eta * zmismatch)
+#define LAMBDA (LIGHT_SPEED/frequency)
+#define FREQUENCY (LIGHT_SPEED/lambda)
+
 #define EFFICIENCY 0.9
-#define PI 3.14159265358979323846
 #define TXT_FILE "inrushIData.txt"
 
 /**
