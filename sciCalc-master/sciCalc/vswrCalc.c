0faeb6e9202134716fa48c83e29605b74befb280
jrom876 <jrom876@gmail.com>
2025-01-08 09:37:09 -0700

    Update vswrCalc.c

--- sciCalc-master/sciCalc/vswrCalc.c
+++ sciCalc-master/sciCalc/vswrCalc.c
@@ -34,8 +34,8 @@
 #include "vswrCalc.h"
 
 /// STANDARD DEFINITIONS FOR PROJECT SCICALC 
-#define PI	3.14159265358979323846 	// ad infinitum sine repeto
-#define LIGHT_SPEED		299792458.0 	// meters per second
+#define PI		3.14159265358979323846 	// ad infinitum sine repeto
+#define LIGHT_SPEED	299792458.0 		// meters per second
 #define STACK_OVERFLOW	2147483648		// Hex 0x80000000
 #define DATA_SIZE 1000
 #define DELTA 1.0e-6
@@ -54,13 +54,13 @@
 
 /// STANDARD DEFINITIONS FOR LIGHT INTENSITY AND ELECTRIC FIELD CALCULATIONS
 #define AIR_REFRACTIVE_INDEX 1.00027717
-#define E0 8.8541878128*PICO				// Permittivity of Free Space in Farads per meter
-#define MU0 1.25663706212*MICRO				// Permeability of Free Space in Newtons per square meter
+#define E0 8.8541878128*PICO			// Permittivity of Free Space in Farads per meter
+#define MU0 1.25663706212*MICRO			// Permeability of Free Space in Newtons per square meter
 #define EPSILON_0 1/(MU0*(LIGHT_SPEED**2))	// Permittivity of Free Space Equation
 #define E_CONSTANT 1/(4*PI*EPSILON_0)
-#define ELECTRON_CHARGE 1.6e-19 			// Charge of an electron in Coulombs
-#define RADIUS_HELIUM_ATOM 26.5e-12			// Radius of a Helium atom in meters
-#define LED_ARRAY_RADIUS 0.35 				// meters from LED array to sample plate
+#define ELECTRON_CHARGE 1.6e-19 		// Charge of an electron in Coulombs
+#define RADIUS_HELIUM_ATOM 26.5e-12		// Radius of a Helium atom in meters
+#define LED_ARRAY_RADIUS 0.35 			// meters from LED array to sample plate
 #define ETA_T (eta * zmismatch)
 #define LAMBDA (LIGHT_SPEED/frequency)
 #define FREQUENCY (LIGHT_SPEED/lambda)
